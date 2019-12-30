e6306e5182511541568fc6f758708d99ae7ea07c
Alex <awmiklovic@gmail.com>
2019-12-30 00:19:58 -0800

    Fixed logic around identifying which nodes need to be styled

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const elementArray = document.querySelectorAll('*, pre, code, span');
+let elementArray = document.querySelectorAll('*');
+elementArray = Array.from(elementArray).filter(el => {
+    // We want to know if this level has text
+    let hasText = false;
+    Array.from(el.childNodes).forEach((child) => {
+        console.log(child);
+        console.log(child.nodeType);
+        if (child.nodeType === 3) hasText = true;
+    });
+    return hasText;
+});
 
 const colorArray = ['#ff0000', '#00ff00', '#0000ff', '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080'];
 
@@ -24,6 +34,8 @@ elementArray.forEach(element => {
     const colorIdx = Array.from(uniqueFamilySet).indexOf(family);
 
     element.style.color = colorArray[colorIdx];
+    // Tooltips don't work on disabled buttons.
+    if (element.hasAttribute('disabled')) element.removeAttribute('disabled');
     window.tippy(element, {
         content: `\n
         <div>Family: ${family}</div>\n
